b7ef8bccd0940e54e5912301f99ae762cf94d378
annkit24 <57790786+annkit24@users.noreply.github.com>
2021-06-07 15:44:53 +0530

    Create MergeSort.cpp

---
+++ Recursion/MergeSort.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+using namespace std;
+
+void merge(int *a,int s,int e){
+
+    int mid=(s+e)/2;
+    int i=s;
+    int j=mid+1;
+    int k=s;
+    int temp[100];
+
+    while(i<=mid && j<=e){
+        if(a[i]<a[j]){
+            temp[k++]=a[i++];
+        }else{
+            temp[k++]=a[j++];
+        }
+    }
+
+    while(i<=mid){
+        temp[k++]=a[i++];
+    }
+    while(j<=e){
+        temp[k++]=a[j++];
+    }
+ 
+  for(int i=s;i<=e;i++){
+        a[i]=temp[i];
+    }
+
+
+}
+
+
+void mergesort(int a[],int s,int e){
+
+if(s>=e){
+    return;
+}
+    int mid=(s+e)/2;
+
+    mergesort(a,s,mid);
+    mergesort(a,mid+1,e);
+    merge(a,s,e);
+}
+int main() {
+	int a[]={4,2,6,1,3};
+	int n=sizeof(a)/sizeof(int);
+	mergesort(a,0,n-1);
+	for(int i=0;i<n;i++){
+	    cout<<a[i]<<" ";
+	}
+	return 0;
+}
